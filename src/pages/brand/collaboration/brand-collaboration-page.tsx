d5b45f005d985d2f74217af4dbd99c7e34ca768f
Piyush1087 <piyush.p1087@gmail.com>
2026-08-11 00:38:15 +0530

    refactor(collaboration): route Brand to shared workspace

--- src/pages/brand/collaboration/brand-collaboration-page.tsx
+++ src/pages/brand/collaboration/brand-collaboration-page.tsx
@@ -1,27 +1,5 @@
-import { Alert } from "../../../design-system/aurora";
-import { PublicBrandLandingWorkspace } from "../../../features/public-brand/components/PublicBrandLandingWorkspace";
-import { useBrandCollaborationPage } from "../../../features/public-brand/hooks/use-brand-collaboration-page";
-import "../../../features/public-brand/public-brand.css";
+import { CollaborationWorkspace } from "../../../features/collaboration/components/CollaborationWorkspace";
 
 export function BrandCollaborationPage() {
-  const { landing, loading, error } = useBrandCollaborationPage();
-
-  if (!loading && error && !landing) {
-    return (
-      <div className="cc-workspace">
-        <Alert tone="error" title="Could not load creator page">
-          {error}
-        </Alert>
-      </div>
-    );
-  }
-
-  return (
-    <PublicBrandLandingWorkspace
-      landing={landing}
-      loading={loading}
-      error={error}
-      layout="brand-preview"
-    />
-  );
+  return <CollaborationWorkspace />;
 }
