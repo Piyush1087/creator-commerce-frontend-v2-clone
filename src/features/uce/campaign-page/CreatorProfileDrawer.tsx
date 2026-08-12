424638a3530808d459deae0ca6ec06cc065e77f7
Piyush1087 <piyush.p1087@gmail.com>
2026-08-12 19:18:59 +0530

    Use Aurora public SideDrawer export

--- src/features/uce/campaign-page/CreatorProfileDrawer.tsx
+++ src/features/uce/campaign-page/CreatorProfileDrawer.tsx
@@ -1,5 +1,4 @@
-import { Alert, Badge, Button } from "../../../design-system/aurora";
-import { SideDrawer } from "../../../design-system/aurora/components/SideDrawer";
+import { Alert, Badge, Button, SideDrawer } from "../../../design-system/aurora";
 import type { CreatorProfileView } from "./types";
 
 export function CreatorProfileDrawer({
